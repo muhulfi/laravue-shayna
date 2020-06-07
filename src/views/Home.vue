b3d23af8288027b9f38069c96300434c6a54edc8
muhulfi <mulfi07@gmail.com>
2020-06-07 18:44:32 +0700

    added home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderShayna />
+    <HeroShayna />
+    <BannerShayna />
+    <InstaShayna />
+    <PartnerLogoShayna />
+    <FooterShayna />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroShayna from '@/components/HeroShayna.vue'
+import BannerShayna from '@/components/BannerShayna.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import PartnerLogoShayna from '@/components/PartnerLogoShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HeaderShayna,
+    HeroShayna,
+    BannerShayna,
+    InstaShayna,
+    PartnerLogoShayna,
+    FooterShayna
+    //HelloWorld
   }
 }
 </script>
